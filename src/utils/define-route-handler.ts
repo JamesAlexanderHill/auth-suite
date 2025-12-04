25a22110913bc4af243603e4dff6c1dc9a3d5a93
James Hill <info@jamesalexanderhill.com>
2025-12-04 11:50:40 +0000

    copy across define-route-handler

---
+++ src/utils/define-route-handler.ts
@@ -0,0 +1,83 @@
+import { z } from "zod";
+import { error } from "./response";
+
+type PlainSchema = {
+  body?: z.ZodType;
+  query?: z.ZodType;
+  params?: z.ZodType;
+};
+type AugmentedCtx<S> = {
+  body: S extends { body: infer B }
+    ? B extends z.ZodType
+      ? z.infer<B>
+      : undefined
+    : undefined;
+  query: S extends { query: infer Q }
+    ? Q extends z.ZodType
+      ? z.infer<Q>
+      : undefined
+    : undefined;
+  params: S extends { params: infer P }
+    ? P extends z.ZodType
+      ? z.infer<P>
+      : undefined
+    : undefined;
+};
+
+/**
+ * Define a route handler with optional schema validation and protection
+ * 
+ * @param handler The route handler function
+ * @param options Route options
+ * @returns An object containing the wrapped handler and options
+ */
+function defineRouteHandler<
+  const S extends PlainSchema | undefined = undefined
+>(
+  handler: (args: { req: Request; ctx: AugmentedCtx<NonNullable<S>> }) => Promise<Response>,
+  options?: { protected?: boolean; schema?: S },
+) {
+  const wrappedHandler = async ({req, ctx}: {req: Request, ctx: S}) => {
+    const schema = options?.schema;
+    let body: unknown = undefined;
+    let params: unknown = undefined;
+    let query: unknown = undefined;
+
+    if (schema?.body) {
+      const json = await req.json();
+      const parsed = schema.body.safeParse(json);
+      if (!parsed.success) {
+        return error("Invalid body");
+      }
+
+      body = parsed.data;
+    }
+
+		if (schema?.query) {
+			const url = new URL(req.url);
+			const queryParams = Object.fromEntries(url.searchParams.entries());
+			const parsed = schema.query.safeParse(queryParams);
+			if (!parsed.success) {
+				return error("Invalid query params");
+			}
+			query = parsed.data;
+		}
+
+		if (schema?.params) {
+			// TODO get params from request URL. Pattern should follow something like /users/:userId
+		}
+
+    const requestCtx = {
+      ...ctx,
+      body: body as AugmentedCtx<S>["body"],
+      query: query as AugmentedCtx<S>["query"],
+      params: params as AugmentedCtx<S>["params"],
+    };
+
+    return handler({req, ctx: requestCtx});
+  };
+
+  return { handler: wrappedHandler, options };
+}
+
+export default defineRouteHandler;
--- src/utils/route-builder.test.ts
+++ src/utils/route-builder.test.ts
@@ -1,16 +1,36 @@
 import { describe, expect, test } from "bun:test";
 
 import RouteBuilder from "./route-builder";
+import type {TBaseRoutes} from './types';
+import {json} from './response';
+import defineRouteHandler from './define-route-handler';
 
 const STORE = {
-  'example-id': 20,
+  exampleId: 20,
 }
+
+const handleRequest = (routes: TBaseRoutes, request: Request): Response => {
+  // invoke the route handler that matches the given request
+  // make sure that any url params are added to an optional ctx object under params
+  // return a 404 response if no route could be found
+} 
 
 describe("RouteBuilder", () => {
   test("build() returns final route object", async () => {
     const routeBuilder = new RouteBuilder()
-      .get("/test/route", (request: Request) => Promise.resolve(true))
-      .post("/test/route/:id", (request: Request, ) => Promise.resolve(STORE["example-id"]));
+      .get("/test/route", defineRouteHandler(
+        async ({ req, ctx }) => json({
+          message: `OTP has been sent to ${email}`
+        }), {
+          protected: false, // this route is public, will need to add rate-limiting
+          schema: {
+            body: z.object({
+              email: z.uuidv4(),
+            }),
+          },
+        }
+      ))
+      .post("/test/route/:id", async (request: Request, ctx: {params: {id: string}}) => Promise.resolve(json(STORE[ctx.params.id] ?? 0)));
 
     const builtRoute = routeBuilder.build();
 
@@ -19,7 +39,8 @@ describe("RouteBuilder", () => {
     expect(builtRoute["/test/route"]).toHaveProperty('GET');
     expect(builtRoute["/test/route/:id"]).toHaveProperty('POST');
 
-    expect(await builtRoute['/test/route'].GET(new Request('/test/route'))).toEqual(true);
+    const res = await handleRequest(builtRoute, new Request('/test/route'))
+    expect(res.json()).toEqual(true);
     expect(await builtRoute['/test/route/:id'].POST(new Request('/test/route/example-id'))).toEqual(STORE["example-id"])
   });
 });
